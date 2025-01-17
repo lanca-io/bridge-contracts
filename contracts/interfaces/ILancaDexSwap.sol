d6460d947d6393d7f83a1798fa60e8c72bbdb251
Aleksei Gerasev <alexey@concero.io>
2025-01-17 16:30:18 +0300

    change swap sig

--- contracts/interfaces/ILancaDexSwap.sol
+++ contracts/interfaces/ILancaDexSwap.sol
@@ -60,5 +60,9 @@ interface ILancaDexSwap {
      * @param recipient the address to send the output token to
      * @return dstTokenReceived the amount of token received after the swap
      */
-    function swap(SwapData[] calldata swapData, address recipient) external payable;
+    function swap(
+        SwapData[] calldata swapData,
+        address receiver,
+        Integration calldata integration
+    ) external payable;
 }
