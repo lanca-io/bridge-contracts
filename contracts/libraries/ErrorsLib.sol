826c535607dba19057f63e00dbc5514e77d979aa
Aleksei Gerasev <alexey@concero.io>
2025-01-21 22:07:01 +0300

    add ErrorsLib

---
+++ contracts/libraries/ErrorsLib.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+library ErrorsLib {
+    enum InvalidAddressType {
+        zeroAddress,
+        notMessenger,
+        unsupportedCcipToken
+    }
+
+    /// @dev Reverts when the address is invalid.
+    error InvalidAddress(InvalidAddressType errorType);
+}
