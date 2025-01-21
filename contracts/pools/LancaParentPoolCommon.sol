ac4e4b2a0d2d03d804423967f7d280612cbd42b9
Aleksei Gerasev <alexey@concero.io>
2025-01-21 16:17:19 +0300

    move immutables and modifiers in LancaPoolCommon

--- contracts/pools/LancaParentPoolCommon.sol
+++ contracts/pools/LancaParentPoolCommon.sol
@@ -3,17 +3,13 @@ pragma solidity 0.8.28;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {LPToken} from "../LPToken.sol";
+import {LancaPoolCommon} from "./LancaPoolCommon.sol";
 
 /**
  * @title LancaParentPoolCommon
  * @notice This contract is the base for all the parent pool contracts.
  */
-contract LancaParentPoolCommon {
-    /**
-     * @notice Thrown when the caller is not an approved messenger.
-     */
-    error NotMessenger();
-
+contract LancaParentPoolCommon is LancaPoolCommon {
     /**
      * @notice The USDC token decimals.
      */
@@ -23,11 +19,6 @@ contract LancaParentPoolCommon {
      * @notice The LP token decimals.
      */
     uint256 internal constant LP_TOKEN_DECIMALS = 1 ether;
-
-    /**
-     * @notice The precision handler for calculations.
-     */
-    uint256 internal constant PRECISION_HANDLER = 1e10;
 
     /**
      * @notice The maximum number of deposits on the way.
@@ -50,48 +41,24 @@ contract LancaParentPoolCommon {
     address internal immutable i_parentPoolProxy;
 
     /**
-     * @notice The USDC token contract.
-     */
-    IERC20 internal immutable i_USDC;
-
-    /**
-     * @notice The approved messengers.
-     */
-    address internal immutable i_msgr0;
-    address internal immutable i_msgr1;
-    address internal immutable i_msgr2;
-
-    /**
-     * @notice Modifier to check if the caller is the an approved messenger.
-     */
-    modifier onlyMessenger() {
-        require(_isMessenger(msg.sender), NotMessenger());
-        _;
-    }
-
-    /**
      * @notice Constructor for the LancaParentPoolCommon contract.
      * @param parentPool the parent pool proxy contract address.
      * @param lpToken the LP token contract address.
      * @param usdc the USDC token contract address.
      * @param messengers the approved messengers.
      */
-    constructor(address parentPool, address lpToken, address usdc, address[3] memory messengers) {
+    constructor(
+        address parentPool,
+        address lpToken,
+        address usdc,
+        address[3] memory messengers,
+        address owner
+    ) LancaPoolCommon(usdc, messengers, owner) {
         i_parentPoolProxy = parentPool;
         i_lpToken = LPToken(lpToken);
-        (i_msgr0, i_msgr1, i_msgr2) = messengers;
-        i_USDC = IERC20(usdc);
     }
 
-    /**
-     * @notice Internal function to check if a caller address is an allowed messenger
-     * @param messenger the address of the caller
-     * @return allowed true if the caller is an allowed messenger, false otherwise
-     */
-    function _isMessenger(address messenger) internal view returns (bool) {
-        return (messenger == i_msgr0 || messenger == i_msgr1 || messenger == i_msgr2);
-    }
-
+    /* INTERNAL FUNCTIONS */
     /**
      * @notice Internal function to convert USDC Decimals to LP Decimals
      * @param usdcAmount the amount of USDC
