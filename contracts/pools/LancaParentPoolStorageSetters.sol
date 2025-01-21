b6331be5fcc26fb53d6757f8bed85482942dab72
Aleksei Gerasev <alexey@concero.io>
2025-01-21 17:12:00 +0300

    remove setters for immutables

--- contracts/pools/LancaParentPoolStorageSetters.sol
+++ contracts/pools/LancaParentPoolStorageSetters.sol
@@ -2,33 +2,10 @@
 pragma solidity 0.8.28;
 
 import {LancaParentPoolStorage} from "../storages/LancaParentPoolStorage.sol";
+import {LancaOwnable} from "../LancaOwnable.sol";
 
-abstract contract LancaParentPoolStorageSetters is LancaParentPoolStorage {
-    function setDonHostedSecretsSlotId(uint8 slotId) external payable onlyOwner {
-        s_donHostedSecretsSlotId = slotId;
-    }
-
-    /**
-     * @notice Function to set the Don Secrets Version from Chainlink Functions
-     * @param version the version
-     * @dev this functions was used inside of ConceroFunctions
-     */
-    function setDonHostedSecretsVersion(uint64 version) external payable onlyOwner {
-        s_donHostedSecretsVersion = version;
-    }
-
-    /**
-     * @notice Function to set the Source JS code for Chainlink Functions
-     * @param hashSum  the JsCode
-     * @dev this functions was used inside of ConceroFunctions
-     */
-    function setCollectLiquidityJsCodeHashSum(bytes32 hashSum) external payable onlyOwner {
-        s_collectLiquidityJsCodeHashSum = hashSum;
-    }
-
-    function setRedistributeLiquidityJsCodeHashSum(bytes32 hashSum) external payable onlyOwner {
-        s_distributeLiquidityJsCodeHashSum = hashSum;
-    }
+abstract contract LancaParentPoolStorageSetters is LancaParentPoolStorage, LancaOwnable {
+    constructor(address owner) LancaOwnable(owner) {}
 
     /**
      * @notice Function to set the Ethers JS code for Chainlink Functions
