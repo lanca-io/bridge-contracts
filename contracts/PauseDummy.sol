6fc5da4f702c992d02fd6569872ef14b15620282
Aleksei Gerasev <alexey@concero.io>
2025-01-22 13:24:44 +0300

    add PauseDummy

---
+++ contracts/PauseDummy.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+/// @title Pause Dummy
+/// @dev This contract is a dummy that reverts all calls. It is used in the tests
+/// to simulate a paused contract.
+/// @custom:security-contact security@concero.io
+contract PauseDummy {
+    /// @notice Reverts when the contract is paused.
+    error Paused();
+
+    fallback() external payable {
+        revert Paused();
+    }
+    receive() external payable {
+        revert Paused();
+    }
+}
