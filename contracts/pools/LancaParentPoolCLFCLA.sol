c0e5ab11fee45678d2b54eeafad7d7f5f33592f7
Aleksei Gerasev <alexey@concero.io>
2025-01-30 14:25:42 +0300

    add imports

--- contracts/pools/LancaParentPoolCLFCLA.sol
+++ contracts/pools/LancaParentPoolCLFCLA.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
+import {AutomationCompatible} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ILancaParentPoolCLFCLA} from "../interfaces/pools/ILancaParentPoolCLFCLA.sol";
+import {ILancaParentPool} from "../interfaces/pools/ILancaParentPool.sol";
+import {ErrorsLib} from "../libraries/ErrorsLib.sol";
 
-contract LancaParentPoolCLFCLA {}
+contract LancaParentPoolCLFCLA is ILancaParentPoolCLFCLA, FunctionsClient, AutomationCompatible {
+    using SafeERC20 for IERC20;
+    using ErrorsLib for *;
+}
