e5bf8d899e9cb6c8327728dd2254912004ed7a4f
Aleksei Gerasev <alexey@concero.io>
2025-01-30 13:57:20 +0300

    add LancaParentPoolCLFCLA

---
+++ contracts/pools/LancaParentPoolCLFCLA.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {ILancaParentPoolCLFCLA} from "../interfaces/pools/ILancaParentPoolCLFCLA.sol";
+
+contract LancaParentPoolCLFCLA {}
