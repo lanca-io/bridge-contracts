f47c60a28ab4e3837ebaf8545e29c43b54d78675
Aleksei Gerasev <alexey@concero.io>
2025-01-30 20:15:11 +0300

    fix typecast

--- contracts/pools/LancaLoan.sol
+++ contracts/pools/LancaLoan.sol
@@ -10,10 +10,10 @@ import {ZERO_ADDRESS} from "../Constants.sol";
 abstract contract LancaLoan is LancaPoolStorage {
     using SafeERC20 for IERC20;
 
-    address internal immutable i_usdc;
+    IERC20 internal immutable i_usdc;
 
     constructor(address usdc) {
-        i_usdc = usdc;
+        i_usdc = IERC20(usdc);
     }
 
     function takeLoan(address token, uint256 amount, address receiver) external payable {
