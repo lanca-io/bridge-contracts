c76eee107c726bde2e4704fbf7df46a01f771f15
Aleksei Gerasev <alexey@concero.io>
2025-02-06 20:29:39 +0300

    add LancaParentPoolMock

---
+++ test/foundry/mocks/LancaParentPoolMock.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {LibLanca} from "../../contracts/common/libraries/LibLanca.sol";
+
+contract LancaParentPoolMock is LancaParentPool {
+    constructor(
+        LibLanca.Token memory token,
+        LibLanca.Addr memory addr,
+        LibLanca.Clf memory clf,
+        LibLanca.Hash memory hash,
+        address[3] memory messengers
+    ) LancaParentPool(token, addr, clf, hash, messengers) {}
+
+    /* SETTERS */
+
+    function setLiquidityCap(uint256 newLiquidityCap) external {
+        s_liquidityCap = newLiquidityCap;
+    }
+
+    function setDepositFeeAmount(uint256 newDepositFeeAmount) external {
+        s_depositFeeAmount = newDepositFeeAmount;
+    }
+
+    function setWithdrawAmountLocked(uint256 newWithdrawAmountLocked) external {
+        s_withdrawAmountLocked = newWithdrawAmountLocked;
+    }
+}
