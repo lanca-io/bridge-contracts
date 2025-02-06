bebc89038906ba2bdb4a574c7d8008f420f58d4b
Aleksei Gerasev <alexey@concero.io>
2025-02-06 22:22:01 +0300

    fix tests

--- test/foundry/src/pools/LancaParentPool.t.sol
+++ test/foundry/src/pools/LancaParentPool.t.sol
@@ -2,10 +2,13 @@
 pragma solidity 0.8.28;
 
 import {Test} from "forge-std/src/Test.sol";
+import {Vm} from "forge-std/src/Vm.sol";
 import {console} from "forge-std/src/console.sol";
 import {LancaParentPool} from "contracts/pools/LancaParentPool.sol";
 import {ILancaParentPool} from "contracts/pools/interfaces/ILancaParentPool.sol";
 import {DeployLancaParentPoolHarnessScript} from "../../scripts/DeployLancaParentPoolHarness.s.sol";
+import {LancaParentPoolMock} from "../../mocks/LancaParentPoolMock.sol";
+import {LancaParentPoolCLFCLAMock} from "../../mocks/LancaParentPoolCLFCLA.sol";
 
 contract LancaParentPoolTest is Test {
     DeployLancaParentPoolHarnessScript internal s_deployLancaParentPoolHarnessScript;
@@ -15,64 +18,84 @@ contract LancaParentPoolTest is Test {
 
     uint256 internal constant USDC_DECIMALS = 1e6;
     uint256 internal constant DEPOSIT_AMOUNT = 100 * USDC_DECIMALS;
+    uint256 internal constant LOW_DEPOSIT_AMOUNT = 1 * USDC_DECIMALS;
+    uint256 internal constant DEPOSIT_DEADLINE_SECONDS = 60;
 
     modifier dealUsdcTo(address to, uint256 amount) {
         deal(s_usdc, to, amount);
+        _;
     }
 
     function setUp() public {
-        vm.createSelectFork(vm.envString("RPC_URL_BASE"), 26000933);
+        uint256 forkId = vm.createSelectFork(vm.envString("RPC_URL_BASE"), 26000933);
         s_deployLancaParentPoolHarnessScript = new DeployLancaParentPoolHarnessScript();
-        s_lancaParentPool = LancaParentPool(payable(s_deployLancaParentPoolHarnessScript.run()));
+        s_lancaParentPool = LancaParentPoolMock(
+            payable(s_deployLancaParentPoolHarnessScript.run(forkId))
+        );
     }
 
     function test_lancaParentPoolStartDepositFailsWhenAmountToLow()
         external
-        dealUsdcTo(depositor, 1 * USDC_DECIMALS)
+        dealUsdcTo(depositor, LOW_DEPOSIT_AMOUNT)
     {
         vm.prank(depositor);
         vm.expectRevert(ILancaParentPool.DepositAmountBelowMinimum.selector);
-        s_lancaParentPool.startDeposit(depositAmount);
+        s_lancaParentPool.startDeposit(LOW_DEPOSIT_AMOUNT);
     }
 
     function test_lancaParentPoolStartDepositFailsWhenMaxDepositCapReached()
         external
         dealUsdcTo(depositor, 100 * USDC_DECIMALS)
     {
-        s_lancaParentPool.setLiquidityCap(1 * USDC_DECIMALS);
+        s_lancaParentPool.setLiquidityCap(LOW_DEPOSIT_AMOUNT);
 
         vm.prank(depositor);
         vm.expectRevert(ILancaParentPool.MaxDepositCapReached.selector);
-        s_lancaParentPool.startDeposit(depositAmount);  
+        s_lancaParentPool.startDeposit(DEPOSIT_AMOUNT);
     }
 
     function test_lancaParentPoolStartDepositSucceeds()
         external
         dealUsdcTo(depositor, 100 * USDC_DECIMALS)
     {
-        uint256 
         vm.prank(depositor);
 
         bytes[] memory args = new bytes[](3);
-        args[0] = abi.encodePacked(s_getChildPoolsLiquidityJsCodeHashSum);
-        args[1] = abi.encodePacked(s_ethersHashSum);
-        args[2] = abi.encodePacked(CLFRequestType.startDeposit_getChildPoolsLiquidity);
-        bytes32 clfRequestId = bytes32(args);
+        args[0] = abi.encodePacked(bytes32(0));
+        args[1] = abi.encodePacked(bytes32(0));
+        args[2] = abi.encodePacked(
+            ILancaParentPool.CLFRequestType.startDeposit_getChildPoolsLiquidity
+        );
+        bytes memory delegateCallArgs = abi.encodeWithSelector(
+            LancaParentPoolCLFCLAMock.sendCLFRequest.selector,
+            args
+        );
+        bytes32 clfRequestId = bytes32(delegateCallArgs);
 
         vm.recordLogs();
 
         s_lancaParentPool.startDeposit(DEPOSIT_AMOUNT);
 
         assertEq(
-            s_clfRequestTypes[clfRequestId],
-            CLFRequestType.startDeposit_getChildPoolsLiquidity
+            uint8(s_lancaParentPool.s_clfRequestTypes(clfRequestId)),
+            uint8(ILancaParentPool.CLFRequestType.startDeposit_getChildPoolsLiquidity)
         );
-        assertEq(s_depositRequests[clfRequestId].lpAddress, depositor);
-        assertEq(s_depositRequests[clfRequestId].usdcAmountToDeposit, DEPOSIT_AMOUNT);
-        assertEq(s_depositRequests[clfRequestId].deadline, block.timestamp + DEPOSIT_DEADLINE_SECONDS);
+
+        (
+            address lpAddress,
+            uint256 usdcAmountToDeposit,
+            uint256 childPoolsLiquiditySnapshot,
+            uint256 deadline
+        ) = s_lancaParentPool.s_depositRequests(clfRequestId);
+
+        assertEq(lpAddress, depositor);
+        assertEq(usdcAmountToDeposit, DEPOSIT_AMOUNT);
+        assertEq(deadline, block.timestamp + DEPOSIT_DEADLINE_SECONDS);
 
         Vm.Log[] memory entries = vm.getRecordedLogs();
-        assertEq(entries[0].topics[0], keccak256("DepositInitiated(bytes32,address,uint256,uint256)"));
-
+        assertEq(
+            entries[0].topics[0],
+            keccak256("DepositInitiated(bytes32,address,uint256,uint256)")
+        );
     }
 }
