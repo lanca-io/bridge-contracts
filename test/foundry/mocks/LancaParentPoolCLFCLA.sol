fc240e044c5c5c12735eb9a52b8178500dc85822
Aleksei Gerasev <alexey@concero.io>
2025-02-06 21:20:14 +0300

    add LancaParentPoolCLFCLAMock

---
+++ test/foundry/mocks/LancaParentPoolCLFCLA.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+contract LancaParentPoolCLFCLAMock {
+    function sendCLFRequest(bytes memory data) external returns (bytes memory) {
+        return data;
+    }
+}
