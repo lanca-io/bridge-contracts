8ec07e9ab1732ffd72c5ea9482ba85d0ce93df0d
Aleksei Gerasev <alexey@concero.io>
2025-02-07 17:43:05 +0300

    add imports and error

--- test/foundry/mocks/LancaParentPoolCLFCLA.sol
+++ test/foundry/mocks/LancaParentPoolCLFCLA.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {LancaPoolCommonStorage} from "contracts/pools/storages/LancaPoolCommonStorage.sol";
 import {LancaParentPoolStorage} from "contracts/pools/storages/LancaParentPoolStorage.sol";
 
 contract LancaParentPoolCLFCLAMock is LancaPoolCommonStorage, LancaParentPoolStorage {
+    using SafeERC20 for IERC20;
+
+    error InvalidCLFRequestType();
+
     function sendCLFRequest(bytes memory data) external returns (bytes memory) {
         return keccak256(data);
     }
