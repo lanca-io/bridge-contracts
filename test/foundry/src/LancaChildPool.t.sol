dbbdcf7d3fe10d1cdccc0ddde8e2e823568fc15a
vpomoconcero <vladimir@concero.io>
2025-03-18 18:45:16 +0300

    added new 3 tests for LancaChildPool

--- test/foundry/src/LancaChildPool.t.sol
+++ test/foundry/src/LancaChildPool.t.sol
@@ -10,9 +10,14 @@ import {ILancaPool} from "contracts/pools/interfaces/ILancaPool.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ILancaChildPool} from "contracts/pools/interfaces/ILancaChildPool.sol";
 import {console} from "forge-std/src/Console.sol";
+import {Client as LibCcipClient} from "@chainlink/contracts/src/v0.8/ccip/libraries/Client.sol";
+import {ICcip} from "contracts/common/interfaces/ICcip.sol";
+
+import "../../../contracts/bridge/interfaces/ILancaBridge.sol";
 
 contract LancaChildPoolTest is Test {
     uint256 internal constant USDC_DECIMALS = 1e6;
+    bytes32 internal constant ANY_BYTES32 = 0x0000000000000000000000000000000000000000000000000000000000000001;
 
     DeployLancaChildPoolHarnessScript internal s_deployChildPoolHarnessScript;
     LancaChildPoolHarness internal s_lancaChildPool;
@@ -99,21 +104,21 @@ contract LancaChildPoolTest is Test {
 
         vm.prank(s_lancaChildPool.exposed_getLancaBridge());
         s_lancaChildPool.takeLoan(s_usdc, amount, receiver);
-        uint256 beforeBal = s_lancaChildPool.getUsdcLoansInUse();
-        vm.assertEq(beforeBal, amount);
+        uint256 beforeBalance = s_lancaChildPool.getUsdcLoansInUse();
+        vm.assertEq(beforeBalance, amount);
 
         vm.startPrank(s_lancaChildPool.exposed_getLancaBridge());
         IERC20(s_usdc).approve(address(s_lancaChildPool), amount);
-        bytes32 anyValue = 0x0000000000000000000000000000000000000000000000000000000000000001;
         amount = 22 * USDC_DECIMALS;
 
-        s_lancaChildPool.completeRebalancing(anyValue, amount);
-        uint256 afterBal = s_lancaChildPool.getUsdcLoansInUse();
-        vm.assertEq(afterBal, beforeBal - amount);
+        s_lancaChildPool.completeRebalancing(ANY_BYTES32, amount);
+        uint256 afterBalance = s_lancaChildPool.getUsdcLoansInUse();
+        vm.assertEq(afterBalance, beforeBalance - amount);
         vm.stopPrank();
     }
 
     /* REVERTS */
+
     function test_setDstPoolNotOwner_revert() public {
         address pool = makeAddr("pool");
         uint64 chainSelector = 1;
@@ -140,15 +145,15 @@ contract LancaChildPoolTest is Test {
         );
         s_lancaChildPool.takeLoan(s_usdc, amount, receiver);
 
-        bytes32 anyValue = 0x0000000000000000000000000000000000000000000000000000000000000001;
         vm.expectRevert(
             abi.encodeWithSelector(
                 LibErrors.Unauthorized.selector,
                 LibErrors.UnauthorizedType.notLancaBridge
             )
         );
-        s_lancaChildPool.completeRebalancing(anyValue, amount);
+        s_lancaChildPool.completeRebalancing(ANY_BYTES32, amount);
     }
+
     /* SET POOLS */
 
     function test_setDstPoolInvalidAddress_revert() public {
@@ -261,7 +266,7 @@ contract LancaChildPoolTest is Test {
     function test_ccipSendToPoolWithdrawalAlreadyTriggered_revert() public {
         uint64 chainSelector = 1;
         address pool = makeAddr("pool");
-        bytes32 withdrawalRequestId = bytes32(0);
+        bytes32 withdrawalRequestId = ANY_BYTES32;
         uint256 amountToSend = 1 * USDC_DECIMALS;
 
         s_lancaChildPool.exposed_setDstPoolByChainSelector(chainSelector, pool);
@@ -270,6 +275,23 @@ contract LancaChildPoolTest is Test {
         vm.prank(s_lancaChildPool.exposed_getMessengers()[0]);
         vm.expectRevert(ILancaPool.WithdrawalAlreadyTriggered.selector);
         s_lancaChildPool.ccipSendToPool(chainSelector, amountToSend, withdrawalRequestId);
+    }
+
+    function test_ccipSendToPool() public {
+        uint64 chainSelector = 1;
+        address pool = makeAddr("pool");
+        bytes32 withdrawalRequestId = ANY_BYTES32;
+        uint256 amountToSend = 10 * USDC_DECIMALS;
+
+        s_lancaChildPool.exposed_setDstPoolByChainSelector(chainSelector, pool);
+        deal(s_usdc, address(s_lancaChildPool), 10 * USDC_DECIMALS);
+        uint256 beforeBalance = IERC20(s_usdc).balanceOf(address(s_lancaChildPool));
+        vm.assertEq(beforeBalance, amountToSend);
+
+        vm.prank(s_lancaChildPool.exposed_getMessengers()[0]);
+        s_lancaChildPool.ccipSendToPool(chainSelector, amountToSend, withdrawalRequestId);
+        uint256 afterBalance = IERC20(s_usdc).balanceOf(address(s_lancaChildPool));
+        vm.assertEq(afterBalance, 0);
     }
 
     /* LIQUIDATE POOL */
@@ -304,7 +326,28 @@ contract LancaChildPoolTest is Test {
         s_lancaChildPool.liquidatePool(distributeLiquidityRequestId);
     }
 
+    function test_liquidatePoolForTwoPools() public {
+        address pool1 = makeAddr("pool 1");
+        address pool2 = makeAddr("pool 2");
+        uint64 chain1Selector = 1;
+        uint64 chain2Selector = 2;
+
+        vm.prank(s_deployChildPoolHarnessScript.getDeployer());
+        s_lancaChildPool.setDstPool(chain1Selector, pool1);
+        vm.prank(s_deployChildPoolHarnessScript.getDeployer());
+        s_lancaChildPool.setDstPool(chain2Selector, pool2);
+
+        bytes32 distributeLiquidityRequestId = bytes32(0);
+        deal(s_usdc, address(s_lancaChildPool), 1000 * USDC_DECIMALS);
+
+        vm.prank(s_lancaChildPool.exposed_getMessengers()[0]);
+        s_lancaChildPool.liquidatePool(distributeLiquidityRequestId);
+        uint256 afterBalance = IERC20(s_usdc).balanceOf(address(s_lancaChildPool));
+        vm.assertEq(afterBalance, 2);
+    }
+
     /* TAKE LOAN */
+
     function test_takeLoanUnauthorized_revert() public {
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -335,5 +378,81 @@ contract LancaChildPoolTest is Test {
             )
         );
         s_lancaChildPool.takeLoan(makeAddr("usdt"), 0, makeAddr("receiver"));
+    }
+    
+    /* CCIP RECEIVE FROM ROUTER */
+
+    function test_ccipReceiveSettlementWithoutExecutionLayerFails() public {
+        address receiver0 = makeAddr("receiver 0");
+        address receiver1 = makeAddr("receiver 1");
+        address receiver2 = makeAddr("receiver 2");
+
+        ILancaBridge.CcipSettlementTxs[]
+        memory ccipSettlementTxs = new ILancaBridge.CcipSettlementTxs[](3);
+
+        ccipSettlementTxs[0] = ILancaBridge.CcipSettlementTxs({
+        id: keccak256("ccip settlement tx 0"),
+        receiver: receiver0,
+        amount: 100 * USDC_DECIMALS
+        });
+
+        ccipSettlementTxs[1] = ILancaBridge.CcipSettlementTxs({
+        id: keccak256("ccip settlement tx 1"),
+        receiver: receiver1,
+        amount: 200 * USDC_DECIMALS
+        });
+
+        ccipSettlementTxs[2] = ILancaBridge.CcipSettlementTxs({
+        id: keccak256("ccip settlement tx 2"),
+        receiver: receiver2,
+        amount: 300 * USDC_DECIMALS
+        });
+
+        ICcip.CcipSettleMessage memory ccipTxs = ICcip.CcipSettleMessage({
+        ccipTxType: ICcip.CcipTxType.batchedSettlement,
+        data: abi.encode(ccipSettlementTxs)
+        });
+
+        LibCcipClient.EVMTokenAmount[] memory destTokenAmounts = new LibCcipClient.EVMTokenAmount[](
+            1
+        );
+
+        destTokenAmounts[0].token = s_usdc;
+
+        uint256 beforeLancaPoolBalance = IERC20(s_usdc).balanceOf(
+            address(s_lancaChildPool)
+        );
+
+        deal(s_usdc, address(s_lancaChildPool), destTokenAmounts[0].amount);
+
+        address s_lancaBridgeArb = makeAddr("arb lanca bridge");
+        uint64 chainSelector = 1;
+
+        LibCcipClient.Any2EVMMessage memory ccipMessage = LibCcipClient.Any2EVMMessage({
+            messageId: keccak256("ccip message id"),
+            sourceChainSelector: chainSelector,
+            sender: abi.encode(s_lancaBridgeArb),
+            data: abi.encode(ccipTxs),
+            destTokenAmounts: destTokenAmounts
+        });
+
+        vm.prank(s_lancaChildPool.getRouter());
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                LibErrors.Unauthorized.selector,
+                LibErrors.UnauthorizedType.notAllowedSender
+            )
+        );
+        s_lancaChildPool.ccipReceive(ccipMessage);
+
+        s_lancaChildPool.exposed_setDstPoolByChainSelector(chainSelector, s_lancaBridgeArb);
+        vm.prank(s_lancaChildPool.getRouter());
+        s_lancaChildPool.ccipReceive(ccipMessage);
+
+        uint256 afterLancaPoolBalance = IERC20(s_usdc).balanceOf(
+            address(s_lancaChildPool)
+        );
+
+        assertEq(afterLancaPoolBalance, beforeLancaPoolBalance + destTokenAmounts[0].amount);
     }
 }
